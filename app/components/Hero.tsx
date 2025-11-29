7cad13dc019c1fbe7a6d56f7b1e90ac061121e7f
ziaul-hassan <ziaulhasanf@gmail.com>
2025-11-29 12:19:37 +0600

    latest

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -54,9 +54,14 @@ export default function Hero() {
                   >
                     <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                   </svg>
-                  <span className="text-blue-700 font-medium">
-                    linkedin.com/in/munifaaktar
-                  </span>
+                  <a
+                    href="https://www.linkedin.com/in/munifa-akter-a9173a372/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-700 font-medium hover:underline"
+                  >
+                    linkedin.com/in/munifaakter
+                  </a>
                 </div>
 
                 <div className="flex items-center justify-center gap-2">
@@ -92,7 +97,12 @@ export default function Hero() {
             Expert in SEO, Lead Generation, and Social Media, Email Marketing
           </p>
 
-          <button className="px-10 py-4 bg-lime-400 text-gray-900 rounded-full font-bold text-lg hover:bg-lime-300 hover:shadow-2xl transform hover:scale-105 transition-all duration-300">
+          <button
+            onClick={() =>
+              (window.location.href = "https://www.facebook.com/munifa.akter.7")
+            }
+            className="px-10 py-4 bg-lime-400 text-gray-900 rounded-full font-bold text-lg hover:bg-lime-300 hover:shadow-2xl transform hover:scale-105 transition-all duration-300"
+          >
             Contact Us
           </button>
         </div>
